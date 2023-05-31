1bba4a4e807188c6fd9ce4c03d3cf0f7ab1dffec
BattutDev <antoine@battut.dev>
2023-05-31 10:11:45 +0200

    feat(v3): new branding, new UI

---
+++ components/landing/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex items-center flex-col gap-10 h-screen py-24">
+    <LandingTitle />
+    <img
+      class="rounded-full h-40 w-40"
+      src="~/assets/img/profile_picture.png"
+      alt="photo de profil"
+    />
+    <LandingSocials />
+    <LandingTextEdit :names="codeEditStrings" />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="w-16 h-16 text-white animate-pulse md:mt-10 lg:mt-28"
+    >
+      <polyline points="7 13 12 18 17 13"></polyline>
+      <polyline points="7 6 12 11 17 6"></polyline>
+    </svg>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const codeEditStrings = ref<Array<string>>([
+	'Développeur Web',
+	'UX/UI Designer',
+	'Développeur Logiciel',
+]);
+</script>
---
+++ components/skills/stack.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex justify-center items-center rounded-xl">
+    <div
+      class="relative gap-2 rounded-xl flex justify-center items-center flex-row group w-full h-full"
+    >
+      <img :src="url" :alt="name" draggable="false" />
+
+      <h5
+        class="hidden text-white absolute group-hover:block -top-10 bg-black bg-opacity-30 px-3 py-1 rounded-lg"
+      >
+        {{ name }}
+      </h5>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+export type Props = {
+  name: string;
+  url: string;
+  group: 'lang' | 'frameworks' | 'db' | 'tool' | 'service';
+};
+
+defineProps<Props>();
+</script>
